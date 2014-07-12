734a856815dd159ee077d5034fc7281daa53ba0c
Spyrology <spyrology@gmail.com>
2014-07-12 15:42:42 -0400

    Version 1
    Additional functionality added. Style sheet and graphics mostly set.

--- js/app.js
+++ js/app.js
@@ -1,53 +1,99 @@
-$(document).ready(function() {
-	alert("hello");
-	$('.get-projects').submit(function(event){
-		// zero out results if previous search has run
-		$('.results').html('');
-		// get the value of the tags the user submitted
-		var tags = $(this).find("input[name='tags']").val();
-		getProjects(tags);
-	});
-});
-
 var showProjects = function(projects) {
 	
 	// clone our result template code
 	var result = $('.templates .projects').clone();
+	var pLink = projects.proposalURL;
 	
 	// Set the project image in result
-	var pImage = result.find('.image a');
-	pImage.attr('href', projects.proposalURL);
-	pImage.html("<img src=" + projects.imageURL + "/>");
+	var pImage = result.find('.p-image img');
+	var imgLink = result.find('.p-image a');
+	var image = projects.imageURL;
+	pImage.attr('src', image);
+	imgLink.attr('href', pLink);
 
 	// set the project title in result
 	var pTitle = result.find('.p-title a');
-	pTitle.attr('href', projects.proposalURL);
-	pTitle.text(projects.title)
-
-	// set the learn more link in result
-	var pLearn = result.find('.p-full');
-	pLearn.html('<a target="_blank" href='+ projects.proposalURL +'</a></p>')
-
-	// set the fund now link in result
-	var pFund = result.find('.donate');
-	pFund.html('<a target="_blank" href='+ projects.fundURL +'</a></p>');
+	pTitle.attr('href', pLink);
+	pTitle.html(projects.title);
+	pTitle.css({
+		"text-decoration": "none", 
+		"font-weight": "bold", "color": "rgb(226,104,59)"
+	});
 
 	return result;
 };
 
-var getProjects = function (tags) {
+var currentIndex = 0;
 
-	var result = $.ajax({
-		url: "http://api.donorschoose.org/common/json_feed.html?keywords="+tags+"&partiallyFunded=no&highLevelPoverty=true&showSynopsis=true&APIKey=DONORSCHOOSE",
-		dataType: "jsonp",
+var getProjects = function (tags, index) {
+
+	var promise = $.ajax({
 		type: "GET",
-	})
-	.done(function(result){
+		url: "http://api.donorschoose.org/common/json_feed.html",
+		data: {
+			partiallyFunded: "no",
+			keywords: tags,
+			highLevelPoverty: "true",
+			showSynopsis: "true",
+			APIKey: "DONORSCHOOSE",
+			index: currentIndex,
+			max: 12,
+		},
+		dataType: "jsonp",
+	});
+
+	loadMore = function() {
+		$('.footer-nav').hide();
+		$('.results').html('');
+      currentIndex += 12;  //increment index to load more projects
+		getProjects(tags, currentIndex);
+	};
+
+	loadPrevious = function() {
+		$('.footer-nav').hide();
+		$('.results').html('');
+      currentIndex -= 12;  //decrement index to load previous projects
+		getProjects(tags, currentIndex);
+	};
+
+	promise.done(function(result){
 
 		$.each(result.proposals, function(i, item) {
 			var projects = showProjects(item);
 			$('.results').append(projects);
 		});
+	$('.footer-nav').show();
+	$('#load-more').show();
+	if (currentIndex >= 12) {
+		$('#load-previous').show();
+	}
+	else {
+		$('#load-previous').hide();
+	};
 	});
 };
 
+var tags;
+
+$(document).ready(function() {
+	alert("hello");
+	$('.search').on('click', "img", function(event) {
+		// zero out results if previous search has run
+		$('.results').html('');
+		// hide navigation in case previous search has run
+		$('.footer-nav').hide();
+		//reset currentIndex for new searches
+		currentIndex = 0;
+		// get the value of the tags the user submitted, which is derived from the 'id' of the subject icon they've clicked.
+		tags = ($(this).attr('id'));
+		getProjects(tags);
+	});
+
+	$('#load-more').click(function(){
+		loadMore();
+	});
+
+	$('#load-previous').click(function(){
+		loadPrevious();
+	});
+});
